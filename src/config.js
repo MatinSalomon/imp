3ac2c696bd0674195108a293a68a5c3cc9dbd213
Martin Salomón <salomonmartin21@gmail.com>
2022-12-05 14:12:39 -0300

    segundo

--- src/config.js
+++ src/config.js
@@ -3,11 +3,10 @@ config();
 
 export const database = {
   connectionLimit: 10,
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root123",
-  database: process.env.DB_DATABASE || "imp",
+  host: process.env.DATABASE_HOST || "localhost",
+  user: process.env.DATABASE_USER || "root",
+  password: process.env.DATABASE_PASSWORD || "root123",
+  database: process.env.DATABASE_NAME || "imp",
   port: process.env.DB_PORT || 3306
 };
-
 export const port = process.env.PORT || 4000;
